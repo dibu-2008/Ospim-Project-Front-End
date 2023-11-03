05a5247d2e7a23c8c0986111a669c1f6774907bf
luigi-coder <luigicoder28@gmail.com>
2023-11-03 12:00:41 -0300

    Modificacion router login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -16,7 +16,7 @@ const AppRouter = () => {
               <Route path="login" element={<LoginPage/>}/>
               <Route path="register" element={<RegisterPage/>}/>
               <Route path="dashboard" element={
-                <PrivateRoute path="dashboard">
+                <PrivateRoute>
                   {/* DashboardPage es el children */}
                   <DashboardPage/>
                 </PrivateRoute>
