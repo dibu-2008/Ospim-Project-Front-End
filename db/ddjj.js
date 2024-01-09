c237480d28643da3ec6531c27b01b63f88747b13
dbueno <diegobueno.sisa@gmail.com>
2024-01-09 02:29:55 -0300

    saque logs

--- db/ddjj.js
+++ db/ddjj.js
@@ -1,12 +1,6 @@
 module.exports = (req, res, next) => {
   console.log("Middleware - DDJJ - INIT..");
 
-  console.log("Middleware - DDJJ - req.method: " + req.method);
-  console.log("Middleware - DDJJ - req.url: " + req.url);
-  console.log(
-    "Middleware - DDJJ - startsWith: " + req.url.startsWith("/DDJJConsulta?")
-  );
-
   if (req.method === "GET" && req.url.startsWith("/DDJJConsulta?")) {
     var ddjj = req.app.db.__wrapped__.DDJJConsulta;
     var empresaId = req.query.empresaId;
@@ -16,31 +10,17 @@ module.exports = (req, res, next) => {
       "01"
     );
 
-    console.log("Middleware - DDJJ - empresaId: " + empresaId);
-    console.log("Middleware - DDJJ - periodo: " + periodo);
+    //console.log("Middleware - DDJJ - empresaId: " + empresaId);
+    //console.log("Middleware - DDJJ - periodo: " + periodo);
     //periodo = new Date(periodo + "T00:00:00Z");
 
     if (empresaId && periodo) {
       const reg = ddjj.find((element) => {
-        console.log(element.periodo);
-        /*
-        console.log(
-          element.periodo.substring(0, 4) +
-            "---" +
-            element.periodo.substring(5, 7) +
-            "---" +
-            "01"
-        );*/
-
         const fecha = new Date(
           element.periodo.substring(0, 4),
           element.periodo.substring(5, 7) - 1,
           "01"
         );
-        //console.log("Middleware - DDJJ - ddjj.find() - periodo: " + periodo);
-        console.log("Middleware - DDJJ - ddjj.find() - fecha: " + fecha);
-        if (fecha.getTime() === periodo.getTime())
-          console.log("periodo IGUALES");
 
         return (
           element.empresaId == empresaId &&
@@ -49,7 +29,7 @@ module.exports = (req, res, next) => {
       });
 
       if (reg) {
-        console.log("Middleware - DDJJ - reg: " + reg);
+        //console.log("Middleware - DDJJ - reg: " + reg);
         res.status(200).send({ existe: true });
       } else {
         res.status(200).send({ existe: false });
