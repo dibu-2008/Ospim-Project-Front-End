626ce229b50d4b493afba3b4d19e21c67269c242
luigi-coder <luigicoder28@gmail.com>
2024-03-10 20:58:54 -0300

    ...

--- src/pages/dashboard/pages_dashboard/ddjj/alta/DDJJAlta.jsx
+++ src/pages/dashboard/pages_dashboard/ddjj/alta/DDJJAlta.jsx
@@ -105,8 +105,51 @@ export const MisAltaDeclaracionesJuradas = ({
     ObtenerPlantaEmpresas();
   }, []);
 
-  const importarAfiliado = () => {
-    setRowsAltaDDJJ(afiliadoImportado);
+  const importarAfiliado = async () => {
+    const cuiles = afiliadoImportado.map((item) => item.cuil);
+    const cuilesString = cuiles.map((item) => item.toString());
+
+    const cuilesResponse = await axiosDDJJ.validarCuiles(
+      ID_EMPRESA,
+      cuilesString
+    );
+
+    // Si alguno de los cuiles el valor de cuilesValidados es igual a false
+    if (cuilesResponse.some((item) => item.cuilValido === false)) {
+      // imprimir en consola el cuil que tiene el valor de cuilValido igual a false
+      const cuilFallido = cuilesResponse.filter(
+        (item) => item.cuilValido === false
+      );
+      cuilFallido.forEach((item) => {
+        console.log(item.cuil);
+      });
+
+      const mensajesFormateados = cuilFallido
+        .map((item, index) => {
+          return `<p>${item.cuil}</p>`;
+        })
+        .join("");
+
+      Swal.fire({
+        icon: "error",
+        title: "Error de validacion",
+        html: `Cuiles con errores:<br>${mensajesFormateados}<br>`,
+        showConfirmButton: true,
+        confirmButtonText: "Aceptar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+      });
+    } else {
+      // swall de 1 segundo success
+      Swal.fire({
+        icon: "success",
+        title: "Importación exitosa",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+
+      setRowsAltaDDJJ(afiliadoImportado);
+    }
   };
 
   const formatearFecha = (fecha) => {
@@ -415,15 +458,3 @@ export const MisAltaDeclaracionesJuradas = ({
     </div>
   );
 };
-
-// Como debe de venir la respuesa de la API
-
-/* cuilesValidados = [
-    {
-     int nullo que no existe el afiliado
-    }
-]
- */
-
-// int si existe o no en la base de datos
-// validacion validar si el cuil es correcto
