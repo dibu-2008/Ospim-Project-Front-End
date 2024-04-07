7f091e9192b24534b2fccce10023e9fcd545e6e0
luigi-coder <luigicoder28@gmail.com>
2024-04-07 11:36:34 -0300

    Trabajando en AltaUsuarioInterno aun falta

--- src/pages/dashboard/pages_dashboard/alta_usuario_interno/AltaUsuarioInterno.jsx
+++ src/pages/dashboard/pages_dashboard/alta_usuario_interno/AltaUsuarioInterno.jsx
@@ -1,6 +1,6 @@
 import * as locales from "@mui/material/locale";
 import { useState, useEffect, useMemo } from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Tooltip, IconButton, alpha, TextField, Grid, InputAdornment, OutlinedInput } from "@mui/material";
 
 import { Add, Edit, DeleteOutlined, Save, Close } from "@mui/icons-material";
 import AddIcon from "@mui/icons-material/Add";
@@ -10,6 +10,10 @@ import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Close";
 import CloseIcon from "@mui/icons-material/Close";
 import CheckIcon from "@mui/icons-material/Check";
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 import {
   GridRowModes,
@@ -26,6 +30,21 @@ import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
 
 import Swal from "sweetalert2";
 import "./AltaUsuarioInterno.css";
+
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 700,
+  bgcolor: "background.paper",
+  border: "2px solid #1A76D2",
+  boxShadow: 24,
+  p: 4,
+};
 
 const crearNuevoRegistro = (props) => {
   const { setRows, rows, setRowModesModel, volverPrimerPagina } = props;
@@ -75,6 +94,9 @@ export const AltaUsuarioInterno = () => {
     pageSize: 10,
     page: 0,
   });
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const volverPrimerPagina = () => {
     setPaginationModel((prevPaginationModel) => ({
@@ -156,6 +178,16 @@ export const AltaUsuarioInterno = () => {
     }
   };
 
+  const handleGestionClave = (id, row) => () => {
+    setNombre(row.nombre);
+    setApellido(row.apellido);
+    setUsuario(row.descripcion);
+    setEmail(row.email);
+    setClave(row.clave);
+    setRepetirClave(row.clave);
+    handleOpen();
+  };
+
   const processRowUpdate = async (newRow, oldRow) => {
     console.log("processRowUpdate - INIT");
     let bOk = false;
@@ -218,11 +250,72 @@ export const AltaUsuarioInterno = () => {
     await axiosUsuariosInternos.habilitar(id, updatedRow.habilitado);
   };
 
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [email, setEmail] = useState("");
+  const [clave, setClave] = useState("");
+  const [repetirClave, setRepetirClave] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleNombre = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const handleApellido = (e) => {
+    setApellido(e.target.value);
+  };
+
+  const handleUsuario = (e) => {
+    setUsuario(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleClave = (e) => {
+    setClave(e.target.value);
+  };
+
+  const handleRepetirClave = (e) => {
+    setRepetirClave(e.target.value);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    if (clave !== repetirClave) {
+      toast.error("Las claves no coinciden !", {
+        position: "top-right",
+        autoClose: 2000,
+        style: {
+          fontSize: "1rem",
+        },
+      });
+      return; // Para salir de la función sin continuar
+    }
+    const data = {
+      nombre,
+      apellido,
+      usuario,
+      email,
+      clave,
+    };
+    console.log(data);
+  }
+  
+
   const columns = [
     {
       field: "apellido",
       headerName: "Apellido",
-      flex: 1,
+      flex: 2,
       type: "string",
       editable: true,
       headerAlign: "center",
@@ -232,7 +325,7 @@ export const AltaUsuarioInterno = () => {
     {
       field: "nombre",
       headerName: "Nombre",
-      flex: 1,
+      flex: 2,
       type: "string",
       editable: true,
       headerAlign: "center",
@@ -242,7 +335,7 @@ export const AltaUsuarioInterno = () => {
     {
       field: "descripcion",
       headerName: "Usuario",
-      flex: 1,
+      flex: 2,
       type: "string",
       editable: true,
       headerAlign: "center",
@@ -258,29 +351,6 @@ export const AltaUsuarioInterno = () => {
       headerAlign: "center",
       align: "center",
       headerClassName: "header--cell",
-    },
-    {
-      field: "clave",
-      headerName: "Contraseña",
-      flex: 1,
-      type: "string",
-      editable: true,
-      headerAlign: "center",
-      align: "center",
-      headerClassName: "header--cell",
-    },
-    {
-      field: "repetirClave",
-      headerName: "Repetir Contraseña",
-      flex: 1,
-      type: "string",
-      editable: true,
-      headerAlign: "center",
-      align: "center",
-      headerClassName: "header--cell",
-      valueGetter: (params) => {
-        return params.row.clave;
-      },
     },
     {
       field: "rolId",
@@ -343,7 +413,7 @@ export const AltaUsuarioInterno = () => {
           ];
         }
 
-        return [
+        const actions = [
           <GridActionsCellItem
             icon={<EditIcon />}
             label="Edit"
@@ -364,6 +434,19 @@ export const AltaUsuarioInterno = () => {
             color="inherit"
           />,
         ];
+
+        actions.push(
+          <Tooltip title="Gestionar Clave" key="clave" placement="top">
+            <GridActionsCellItem
+              icon={<LockPersonIcon />}
+              label="Gestionar Clave"
+              sx={{ color: "primary.main" }}
+              onClick={handleGestionClave(id, row)}
+            />
+          </Tooltip>
+        );
+
+        return actions;
       },
     },
   ];
@@ -420,6 +503,147 @@ export const AltaUsuarioInterno = () => {
             pageSizeOptions={[5, 10, 25]}
           />
         </ThemeProvider>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        > 
+        
+          <Box sx={style}>
+            <form onSubmit={handleFormSubmit}>
+              <Typography
+                variant="h4"
+                component="h2"
+                sx={{
+                  textAlign: "center",
+                  backgroundColor: alpha(theme.palette.primary.main, 0.1),
+                  borderRadius: "5px",
+                  width: "400px",
+                  marginBottom: "20px",
+                  color: theme.palette.primary.main,
+                }}
+              >
+                Gestion de Clave
+              <ToastContainer />
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    value={nombre}
+                    label="Nombre"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleNombre}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    value={apellido}
+                    label="Apellido"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleApellido}
+                  />
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    value={usuario}
+                    label="Usuario"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleUsuario}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    value={email}
+                    label="email"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleEmail}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <OutlinedInput
+                    value={clave}
+                    label="Clave"
+                    variant="outlined"
+                    fullWidth
+                    margin="dense"
+                    type={showPassword ? "text" : "password"}
+                    onChange={handleClave}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+
+                </Grid>
+                <Grid item xs={6}>
+                  <OutlinedInput
+                    value={repetirClave}
+                    label="Repetir Clave"
+                    variant="outlined"
+                    fullWidth
+                    margin="dense"
+                    type={showPassword ? "text" : "password"}
+                    onChange={handleRepetirClave}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Grid>
+              </Grid>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                sx={{ width: "76%" }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{ marginTop: "20px" }}
+                  type="submit"
+                >
+                  Cambiar Clave
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{ marginTop: "20px" }}
+                  onClick={handleClose}
+                >
+                  Cancelar
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </Modal>
       </Box>
     </div>
   );
