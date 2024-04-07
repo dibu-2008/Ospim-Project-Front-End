bebe706cec735166f55161babb1940eb4dee915d
luigi-coder <luigicoder28@gmail.com>
2024-04-07 14:36:09 -0300

    Se paso a indexOf, se quito InternalId e isNew, solucion del bug de handleCancel

--- src/pages/dashboard/pages_dashboard/cuits-restringidos/CuitsRestringidos.jsx
+++ src/pages/dashboard/pages_dashboard/cuits-restringidos/CuitsRestringidos.jsx
@@ -21,12 +21,9 @@ import { StripedDataGrid, dataGridStyle } from "@/common/dataGridStyle";
 import "./CuitsRestringidos.css";
 
 function EditToolbar(props) {
-  const { setRows, rows, setRowModesModel, volverPrimerPagina } = props;
+  const { setRows, setRowModesModel, volverPrimerPagina } = props;
 
   const altaHandleClick = () => {
-    /* const maxId = rows ? Math.max(...rows.map((row) => row.internalId), 0) : 1;
-    const newId = maxId + 1;
-    const internalId = newId; */
 
     const newReg = { cuit: "", observacion: "" }
 
@@ -77,8 +74,6 @@ export const CuitsRestringidos = () => {
   useEffect(() => {
     const ObtenerCuitsRestringidos = async () => {
       const response = await axiosCuitsRestringidos.consultar();
-      // setRows(response.map((item, index) => ({ internalId: index + 1, ...item })));
-      //setRows(response.map((item) => ({ internalId: item.id, ...item })));
       setRows(response);
     };
     ObtenerCuitsRestringidos();
@@ -137,7 +132,7 @@ export const CuitsRestringidos = () => {
     });
 
     const editedRow = rows.find((reg) => reg.id === row.id);
-    if (editedRow.isNew) {
+    if (!editedRow.id) {
       setRows(rows.filter((reg) => reg.id !== row.id));
     }
   };
@@ -148,15 +143,10 @@ export const CuitsRestringidos = () => {
     if (!newRow.id) {
       console.log("2 - processRowUpdate - ALTA ");
       try {
-        /* const internalId = newRow.internalId;
-        delete newRow.internalId;
-        delete newRow.isNew; */
         const data = await axiosCuitsRestringidos.crear(newRow);
         console.log("data: " + JSON.stringify(data));
         if (data && data.id) {
           newRow.id = data.id;
-          /* newRow.internalId = internalId;
-          newRow.isNew = false; */
           bOk = true;
 
           console.log("ALTA - rows: ");
@@ -175,13 +165,10 @@ export const CuitsRestringidos = () => {
     } else {
       console.log("3 - processRowUpdate - MODI ");
       try {
-        /* const internalId = newRow.internalId;
-        delete newRow.internalId;
-        delete newRow.isNew; */
+
         bOk = await axiosCuitsRestringidos.actualizar(newRow);
         console.log("4 - processRowUpdate - MODI - bOk: " + bOk);
-        /* newRow.isNew = false;
-        newRow.internalId = internalId; */
+     
         if (bOk) {
           const rowsNew = rows.map((row) =>
             row.id === newRow.id ? newRow : row
@@ -307,7 +294,7 @@ export const CuitsRestringidos = () => {
               toolbar: EditToolbar,
             }}
             slotProps={{
-              toolbar: { setRows, rows, setRowModesModel, volverPrimerPagina },
+              toolbar: { setRows, setRowModesModel, volverPrimerPagina }
             }}
             sx={{
               "& .MuiDataGrid-virtualScroller::-webkit-scrollbar": {
