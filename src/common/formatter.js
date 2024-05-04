76bbd8a99895cd52f6a034e29f094a2d9320acc4
dbueno <diegobueno.sisa@gmail.com>
2024-05-04 10:03:21 -0300

    arregle formatter.date(value) para q soporte undefined

--- src/common/formatter.js
+++ src/common/formatter.js
@@ -9,12 +9,15 @@ const formatCurrency = new Intl.NumberFormat('es-CL', {
 
 const formatDate = (value) => {
   try {
-    const date = new Date(value);
+    let strFecha = '';
+    if (value) {
+      const date = new Date(value);
 
-    const day = date.getUTCDate().toString().padStart(2, '0');
-    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-    const year = date.getUTCFullYear();
-    const strFecha = `${day}/${month}/${year}`;
+      const day = date.getUTCDate().toString().padStart(2, '0');
+      const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+      const year = date.getUTCFullYear();
+      strFecha = `${day}/${month}/${year}`;
+    }
     return strFecha;
   } catch (error) {
     console.log(error);
