d2324667690409ad2c1394d7f67c52730f855620
dbueno <diegobueno.sisa@gmail.com>
2024-08-24 13:46:47 -0300

    casteo file

--- src/pages/dashboard/pages_dashboard/ddjj/formulario/DDJJArchivoImport.jsx
+++ src/pages/dashboard/pages_dashboard/ddjj/formulario/DDJJArchivoImport.jsx
@@ -5,7 +5,7 @@ import formatter from '@/common/formatter';
 import Swal from 'sweetalert2';
 import swal from '@/components/swal/swal';
 import { axiosDDJJ } from './DDJJApi';
-
+import dayjs from 'dayjs';
 import localStorageService from '@/components/localStorage/localStorageService';
 
 export const DDJJArchivoImport = ({
@@ -19,18 +19,30 @@ export const DDJJArchivoImport = ({
   console.log('DDJJArchivoImport - habiModif:', habiModif);
   const ID_EMPRESA = localStorageService.getEmpresaId();
   const IMPORTACION_OK = import.meta.env.VITE_IMPORTACION_OK;
+  const VC_CUIL = 'Cuil';
+  const VC_APE = 'Apellido';
+  const VC_NOM = 'Nombre';
+  const VC_CAMARA = 'Camara';
+  const VC_CAT = 'Categoria';
+  const VC_FING = 'Fecha de ingreso';
+  const VC_PLANTA = 'Planta';
+  const VC_REMU = 'Remunerativo';
+  const VC_NREMU = 'No Remunerativo';
+  const VC_SINDICAL = 'Adherido al sindicato';
+  const VC_MUTUAL = 'Paga Mutual';
+
   const vecRowTitulos = [
-    'Cuil',
-    'Apellido',
-    'Nombre',
-    'Camara',
-    'Categoria',
-    'Fecha de ingreso',
-    'Planta',
-    'Remunerativo',
-    'No Remunerativo',
-    'Adherido al sindicato',
-    'Paga Mutual',
+    VC_CUIL,
+    VC_APE,
+    VC_NOM,
+    VC_CAMARA,
+    VC_CAT,
+    VC_FING,
+    VC_PLANTA,
+    VC_REMU,
+    VC_NREMU,
+    VC_SINDICAL,
+    VC_MUTUAL,
   ];
 
   const [fileNameSelected, setFileNameSelected] = useState(''); // validar si eligieron un archivo
@@ -49,21 +61,29 @@ export const DDJJArchivoImport = ({
       reader.onload = (e) => {
         const rows = readFile(e);
 
-        if (!validarTitulos(rows)) {
-          swal.showWarning(
-            '<div><p>Formato de archivo incorrecto.<br>La primera fila debe incluir titulos de las <b>11 columnas</b>:<br><br> 1-CUIL<br>2-Apellido<br>3-Nombre<br>4-Cámara<br>5-Categoría<br>6-Ingreso<br>7-Planta<br>8-Remunerativo<br>9-No Remunerativo<br>10-Adherido Sindicato<br>11-Paga Mutual</p></div>',
-            true,
-          );
-          fileReset();
-          console.log('HACE: event.target.value = null;');
+        console.log('readFile - rows:', rows);
+
+        if (!validarFileColCanti(rows)) {
           event.target.value = null;
-          console.log('HACE: event.target.value = null; => OK');
           return false;
         }
 
-        const rowsDatos = rows.slice(1);
-        console.log('rowsDatos:', rowsDatos);
-        const vecRowsGridDto = castFileRowsToGrid(rowsDatos);
+        if (!validarTitulos(rows)) {
+          event.target.value = null;
+          return false;
+        }
+        rows.shift(); //SACO 1er row: Row Titulo
+        console.log(`shift() - rows: `, rows);
+
+        castearTiposDato(rows);
+
+        if (!validarCuilesVacios(rows)) {
+          event.target.value = null;
+          return false;
+        }
+
+        console.log('rowsDatos:', rows);
+        const vecRowsGridDto = castFileRowsToGrid(rows);
         console.log('vecRowsGridDto:', vecRowsGridDto);
 
         setFileVecCuiles(vecRowsGridDto);
@@ -89,38 +109,59 @@ export const DDJJArchivoImport = ({
     const workbook = XLSX.read(data, { type: 'array' });
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
-    const rows = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+    const rows = XLSX.utils.sheet_to_json(sheet, {
+      header: 1,
+      raw: false,
+      dateNF: 'dd/mm/yyyy',
+    });
 
-    console.log('rows:', rows);
-    let rowUno = [];
-    if (rows && rows.length > 0) {
-      rowUno = rows[0]; //Guardo Row Titulo
-    }
-
-    const rowsClean = rows.filter((reg) => {
-      if (reg?.length > 0 && reg[0] != null && Number.isInteger(reg[0])) {
-        return true;
+    //Elimino rows en blanco
+    console.log('readFile - rows:', rows);
+    let rowsClean = rows.filter((reg) => {
+      if (Array.isArray(reg) && reg?.length > 0) {
+        let rta = false;
+        reg.forEach(function (col, index) {
+          if (typeof col !== 'undefined' && col != null) {
+            rta = true;
+          }
+        });
+        return rta;
       }
-    }); //Elimino rows en blanco y donde no hay un numerico en
-
-    if (rowsClean && rowsClean.length > 0) {
-      if (rowsClean[0].toString() != rowUno.toString()) {
-        //Devuelvo row Titulos
-        rowsClean.unshift(rowUno);
-      }
-    }
-    console.log('**rowsClean:', rowsClean);
+    });
+    console.log('readFile - rowsClean:', rowsClean);
 
     return rowsClean;
   };
 
+  const validarFileColCanti = (vecRows) => {
+    const colCanti = vecRowTitulos.length;
+    let rowError = false;
+    let rowErrorNro = null;
+    vecRows.forEach(function (row, index) {
+      if (!Array.isArray(row) || row.length != colCanti) {
+        rowError = false;
+        rowErrorNro = index;
+      }
+    });
+    if (rowError) {
+      swal.showWarning(
+        `<div><p>Registro Nro. ${rowErrorNro + 1} incompleto. Deb informar 11 columnas</p></div>`,
+        true,
+      );
+      fileReset();
+    }
+    return !rowError;
+  };
+
   const validarTitulos = (vecRows) => {
-    if (vecRows[0].length === 11) {
-      //console.log('vecRows[0].toString(): ', vecRows[0].toString());
-      //console.log('vecRowTitulos.toString(): ', vecRowTitulos.toString());
+    const colCanti = vecRowTitulos.length;
+    if (vecRows[0].length === colCanti) {
       try {
         const vecTitTrim = vecRows[0].map((reg) => reg.trim());
-        if (vecTitTrim.toString() === vecRowTitulos.toString()) {
+        if (
+          vecTitTrim.toString().toUpperCase() ===
+          vecRowTitulos.toString().toUpperCase()
+        ) {
           return true;
         }
       } catch (e) {
@@ -130,7 +171,132 @@ export const DDJJArchivoImport = ({
         } catch (e2) {}
       }
     }
+
+    let msgError =
+      '<div><p>Formato de archivo incorrecto.<br>La primera fila debe incluir los titulos de las <b>11 columnas</b>:<br><br>';
+    vecRowTitulos.map((reg, index) => {
+      msgError += `${index + 1}-${reg}<br>`;
+    });
+
+    msgError = '</p></div >';
+    swal.showWarning(msgError, true);
+    fileReset();
     return false;
+  };
+  const validarCuilesVacios = (vecRows) => {
+    let rowError = false;
+    let rowErrorNro = false;
+    const indexCuil = vecRowTitulos.indexOf(VC_CUIL);
+    vecRows.forEach(function (reg, index) {
+      if (reg[indexCuil] == null) {
+        console.log('ERROR - reg:', reg);
+        rowError = true;
+        rowErrorNro = index;
+      }
+    });
+
+    if (rowError) {
+      swal.showWarning(
+        `<div><p>Falta informa Nro de CUIL en el registro Nro. ${rowErrorNro + 1} </p></div>`,
+        true,
+      );
+      fileReset();
+    }
+    console.log('validarCuilesVacios - vecRows:', vecRows);
+
+    return !rowError;
+  };
+
+  const castearBoolean = (valor) => {
+    if (valor == null || valor == undefined) {
+      return null;
+    }
+    if (typeof valor == 'boolean') return valor;
+
+    if (valor == 'true') {
+      return true;
+    }
+    if (valor == 'false') {
+      return false;
+    } // Returns boolean
+    return null;
+  };
+  const castearString = (valor) => {
+    if (valor == undefined || valor == '') return null;
+
+    if (typeof valor == 'number') return valor.toString();
+
+    return valor;
+  };
+
+  const castearDate = (valor) => {
+    if (valor == null || valor == undefined) {
+      return null;
+    }
+    if (typeof valor == 'string') {
+      if (dayjs(valor, 'DD/MM/YYYY', true).isValid()) {
+        return valor;
+      }
+      if (dayjs(valor, 'DD-MM-YYYY', true).isValid()) {
+        return valor;
+      }
+    }
+    return null;
+  };
+
+  const castearFloat = (valor) => {
+    if (valor == null || valor == undefined) {
+      return null;
+    }
+    if (typeof valor == 'number') return valor;
+    if (typeof valor == 'string') {
+      valor = valor.trim();
+      if (valor == '') return null;
+      if (isNaN(valor)) return null;
+      return parseFloat(valor);
+    }
+  };
+
+  const castearTiposDato = (vecRows) => {
+    console.log(`castearTiposDato - INIT - vecRows: ${vecRows}`);
+    const vecErrores = [];
+    vecRows.forEach(function (reg, index) {
+      console.log(`reg: ${reg} `);
+
+      let index2 = vecRowTitulos.indexOf(VC_CUIL);
+      reg[index2] = castearString(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_SINDICAL);
+      reg[index2] = castearBoolean(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_MUTUAL);
+      reg[index2] = castearBoolean(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_FING);
+      console.log('castearTiposDato - reg[index2]: ', reg[index2]);
+      reg[index2] = castearDate(reg[index2]);
+      console.log('castearTiposDato - reg[index2]: ', reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_REMU);
+      reg[index2] = castearFloat(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_NREMU);
+      reg[index2] = castearFloat(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_APE);
+      reg[index2] = castearString(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_NOM);
+      reg[index2] = castearString(reg[index2]);
+
+      index2 = vecRowTitulos.indexOf(VC_CAMARA);
+      reg[index2] = castearString(reg[index2]);
+      index2 = vecRowTitulos.indexOf(VC_CAT);
+      reg[index2] = castearString(reg[index2]);
+      index2 = vecRowTitulos.indexOf(VC_PLANTA);
+      reg[index2] = castearString(reg[index2]);
+    });
+    console.log(`castearTiposDato - FIN - vecRows:`, vecRows);
   };
 
   const findCodigo = (vector, codigo) => {
@@ -175,7 +341,7 @@ export const DDJJArchivoImport = ({
   };
   const importarAfiliado = async () => {
     //fileVecCuiles: valida los cuiles y  actualiza  nombre y apellido del arcivo con lo que hay en "Afiliados"
-    console.log('importarAfiliado - fileNameSelected:', fileNameSelected);
+    //console.log('importarAfiliado - fileNameSelected:', fileNameSelected);
     if (
       !fileNameSelected ||
       fileNameSelected == '' ||
@@ -187,20 +353,32 @@ export const DDJJArchivoImport = ({
 
     if (!fileVecCuiles || fileVecCuiles.length == 0) {
       //no hay registros
+      console.log('importarAfiliado - fileVecCuiles:', fileVecCuiles);
       swal.showWarning('El archivo seleccionado se encuentra vacío.');
       return false;
     }
 
-    console.log('importarAfiliado - 1 -  getCuilesValidados() ');
+    //console.log('importarAfiliado - 1 -  getCuilesValidados() ');
     const cuilesValidados = await getCuilesValidados();
+    //console.log('importarAfiliado - 1 -  cuilesValidados:', cuilesValidados);
     //1) Si existe cuil y no tiene errores, piso nombre y apellido.-
     //2) Si no existe cuil y tiene error de cuil, seteo gError=true
 
-    console.log('importarAfiliado - 2 -  fileVecCuilesNew');
+    //console.log('importarAfiliado - 2 -  fileVecCuilesNew - fileVecCuiles:',fileVecCuiles,);
     const fileVecCuilesNew = fileVecCuiles.map((item, index) => {
-      const val = cuilesValidados.find(
-        (regValidado) => regValidado.cuil === item.cuil,
-      );
+      const val = cuilesValidados.find((regValidado) => {
+        //console.log('cuilesValidados.find - regValidado:', regValidado);
+        //console.log('cuilesValidados.find - item.cuil:', item.cuil);
+        if (!isNaN(parseInt(regValidado.cuil))) {
+          // console.log(            'cuilesValidados.find - TRUE - if (!isNaN(parseInt(regValidado.cuil))) {',          );
+          //console.log('parseInt(regValidado.cuil):',            parseInt(regValidado.cuil),          );
+          //console.log(            'parseInt(regValidado.cuil) == item.cuil:',            parseInt(regValidado.cuil) == item.cuil,          );
+          parseInt(regValidado.cuil) == item.cuil;
+        }
+        return false;
+      });
+      //console.log('fileVecCuiles.map - val:', val);
+
       if (val) {
         //{cuil; inte; apellido; nombre; cuilValido;}
         if (!val.cuilValido) {
@@ -212,6 +390,7 @@ export const DDJJArchivoImport = ({
       }
       return item;
     });
+    console.log('importarAfiliado - 2 -  fileVecCuilesNew:', fileVecCuilesNew);
 
     console.log('importarAfiliado - 3 -  fileVecCuilesNew - some() ');
     // Si alguno de los cuiles tiene ERROR
